104fcefd517a0a2d901c73b42fe931544ff4a867
ErickCD <acheron_1405@hotmail.com>
2017-08-07 15:30:45 -0500

    Ahora reconoce los metodos vacios

--- Lenguaje_SID/GUI/GUIAFD.java
+++ Lenguaje_SID/GUI/GUIAFD.java
@@ -11,7 +11,6 @@ import java.io.FileReader;
 import java.util.ArrayList;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
-import javax.swing.JTable;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
