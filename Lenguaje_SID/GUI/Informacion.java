de88c60e200ccfa5d6c894de2e79c28f1f1cd90b
Rodrigo-Alejandro <rodrigo.10reyes@hotmail.com>
2017-08-09 22:48:36 -0500

    cambios en la venytana de aserca de

--- Lenguaje_SID/GUI/Informacion.java
+++ Lenguaje_SID/GUI/Informacion.java
@@ -30,13 +30,13 @@ public class Informacion extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-<<<<<<< HEAD
+
         jLabel1 = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
-=======
+
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
->>>>>>> 5b0b803e5c1aa3d4e08b4ee2c8d4b87eb3d541b9
+
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
@@ -49,14 +49,13 @@ public class Informacion extends javax.swing.JFrame {
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
 
-<<<<<<< HEAD
-=======
+
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jPanel1.setBackground(new java.awt.Color(204, 204, 255));
         jPanel1.setForeground(new java.awt.Color(0, 0, 0));
 
->>>>>>> 5b0b803e5c1aa3d4e08b4ee2c8d4b87eb3d541b9
+
         jLabel1.setBackground(new java.awt.Color(0, 0, 0));
         jLabel1.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(0, 0, 0));
@@ -64,7 +63,7 @@ public class Informacion extends javax.swing.JFrame {
         jLabel1.setText("INSTITUTO TECNOLOGICO SUPERIOR DE TANTOYUCA");
         jLabel1.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         jLabel1.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
-<<<<<<< HEAD
+
         jLabel1.getAccessibleContext().setAccessibleDescription("");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
@@ -73,9 +72,7 @@ public class Informacion extends javax.swing.JFrame {
         jPanel1.setForeground(new java.awt.Color(0, 0, 0));
 
         jLabel2.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
-=======
 
->>>>>>> 5b0b803e5c1aa3d4e08b4ee2c8d4b87eb3d541b9
         jLabel2.setForeground(new java.awt.Color(51, 51, 51));
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel2.setText("LENGUAJES Y AUTOMATAS II");
@@ -139,10 +136,9 @@ public class Informacion extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-<<<<<<< HEAD
-=======
+
                             .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 516, Short.MAX_VALUE)
->>>>>>> 5b0b803e5c1aa3d4e08b4ee2c8d4b87eb3d541b9
+
                             .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -174,13 +170,13 @@ public class Informacion extends javax.swing.JFrame {
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-<<<<<<< HEAD
+
                 .addGap(50, 50, 50)
-=======
+
                 .addContainerGap()
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 38, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
->>>>>>> 5b0b803e5c1aa3d4e08b4ee2c8d4b87eb3d541b9
+
                 .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel3)
@@ -206,11 +202,10 @@ public class Informacion extends javax.swing.JFrame {
                 .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 24, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
-<<<<<<< HEAD
-=======
+
         jLabel1.getAccessibleContext().setAccessibleDescription("");
 
->>>>>>> 5b0b803e5c1aa3d4e08b4ee2c8d4b87eb3d541b9
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
